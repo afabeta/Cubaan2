c35d38bf74933e55474e514a2b40f27e598e0c5b
Fahimi <fahimizta99@gmail.com>
2022-08-10 14:52:53 +0800

    Web Scam !

---
+++ script.js
@@ -0,0 +1,45 @@
+// window.onload=function(){var Name = document.getElementById('userName');
+// console.log(Name.value)
+
+// Name.onchange = function(){
+//     console.log(Name.value)
+// }
+// }
+
+document.addEventListener("click", myFunction);
+
+function myFunction() {
+    const Name = document.querySelector('#userName');
+    const IcNumber = document.querySelector('#icNum')
+    const Gender = document.querySelector('#userGen');
+    const AccNumber = document.querySelector('#accNum');
+ console.log(Name.value)
+ console.log(IcNumber.value)
+ console.log(Gender.value)
+ console.log(AccNumber.value)
+}
+
+
+
+// window.onload=function(){var IcNumber = document.getElementById('icNum');
+// console.log(IcNumber.value)
+
+// IcNumber.onchange = function(){
+//     console.log(IcNumber.value)
+// }
+// }
+// window.onload=function(){var Gender = document.getElementById('userGen');
+// console.log(Gender.value)
+
+// Gender.onchange = function(){
+//     console.log(Gender.value)
+// }
+// }
+// window.onload=function(){var AccNumber = document.getElementById('accNum');
+// console.log(AccNumber.value)
+
+// AccNumber.onchange = function(){
+//     console.log(AccNumber.value)
+// }
+// }
+
